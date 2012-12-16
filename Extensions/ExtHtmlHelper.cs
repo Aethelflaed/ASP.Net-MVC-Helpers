7519d728646dd5c0b6c6f51797cbefd1682c5248
Geoffroy Planquart <geoffroy@windows8>
2012-12-16 19:47:03 +0100

    Added DropDownForEnum HtmlHelper

---
+++ Extensions/ExtHtmlHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aethelflaed.Extensions
+{
+	public static class ExtHtmlHelper
+	{
+		public static MvcHtmlString DropDownForEnum<T, U>(this HtmlHelper<T> h, Expression<Func<T, U>> func) where T : new()
+		{
+			Type enumType = typeof(U);
+
+			T model = h.ViewData.Model;
+			if (model == null)
+			{
+				model = new T();
+			}
+			string name = ((MemberExpression)func.Body).Member.Name;
+			U selectedValue = func.Compile()(model);
+
+			ExtTagBuilder tag = new ExtTagBuilder("select").With("name", name).And("id", name);
+
+			foreach (U val in Enum.GetValues(enumType))
+			{
+				//string enumText = Resources.ResourceManager.GetString(val.ToString());
+				//if (String.IsNullOrEmpty(enumText)) enumText = val.ToString();
+				string enumText = val.ToString();
+				ExtTagBuilder option = new ExtTagBuilder("option").With("value", (val).ToString()).AndIf(val.Equals(selectedValue), "selected", "selected").WithText(enumText);
+				tag.Append(option);
+			}
+			return MvcHtmlString.Create(tag.ToString());
+		}
+	}
+}
---
+++ Extensions/ExtTagBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aethelflaed.Extensions
+{
+	public class ExtTagBuilder : TagBuilder
+	{
+		public ExtTagBuilder(string TagName)
+			: base(TagName)
+		{
+
+		}
+
+		public ExtTagBuilder Append(ExtTagBuilder innerTag)
+		{
+			base.InnerHtml += innerTag.ToString();
+			return this;
+		}
+
+		public ExtTagBuilder WithText(string text)
+		{
+
+			base.InnerHtml += text;
+			return this;
+		}
+
+		public ExtTagBuilder With(ExtTagBuilder innerTag)
+		{
+			base.InnerHtml = innerTag.ToString();
+			return this;
+		}
+
+		public ExtTagBuilder With(string attributeName, string attributeValue)
+		{
+			base.Attributes.Add(attributeName, attributeValue);
+			return this;
+		}
+
+		public ExtTagBuilder And(string attributeName, string attributeValue)
+		{
+			base.Attributes.Add(attributeName, attributeValue);
+			return this;
+		}
+
+
+		public ExtTagBuilder AndIf(bool condition, string attributeName, string attributeValue)
+		{
+			if (condition)
+				base.Attributes.Add(attributeName, attributeValue);
+			return this;
+		}
+
+	}
+}
